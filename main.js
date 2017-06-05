17516f93a9736b96fddf9e40ff5e36ccb2279b40
Andrew Weaver <weevz@uw.edu>
2017-06-05 14:56:12 -0700

    window size

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 300, height: 300,frame: false,resizable: false})
+  mainWindow = new BrowserWindow({width: 300, height: 600,frame: false,resizable: false})
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
