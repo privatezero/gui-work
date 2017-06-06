6441aab13f4bf018dcbe5da140c3772de3479a48
Andrew Weaver <weevz@uw.edu>
2017-06-05 19:55:09 -0700

    make movable

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 300, height: 600,frame: false,resizable: false})
+  mainWindow = new BrowserWindow({width: 300, height: 600,resizable: false})
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
