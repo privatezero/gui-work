e0e1f8a391becfe101a37d24a87a5fa5c69fd543
Ashley Blewer <ashley.blewer@gmail.com>
2018-12-09 19:16:03 -0500

    Clean up from devtools

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1000, height: 700, resizable: true})
+  mainWindow = new BrowserWindow({width: 600, height: 700, resizable: false})
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
@@ -23,7 +23,7 @@ function createWindow () {
   }))
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
